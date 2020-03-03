10976ae43d1d5d56a0656bb7c7343c3be281d429
faisal <faizal202@gmail.com>
2020-03-02 23:32:21 -0500

    Created tools controller view and index view.

---
+++ ToolLending/Controllers/ToolsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToolLending.Models;
+
+namespace ToolLending.Controllers
+{
+    public class ToolsController : Controller
+    {
+        // GET: Tools/
+        public ActionResult Index()
+        {
+            Tool tool = new Tool() { Id = 0, Name = "Screw Driver"};
+            return View(tool);
+        }
+    }
+}
